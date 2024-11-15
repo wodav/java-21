0644c4ca4e957e5bcc3c37ec4707aa71d957e4ab
wodav <74779169+wodav@users.noreply.github.com>
2024-11-15 10:56:30 +0000

    feature/jep-440-record-patterns: Add ExtendedUser record

--- RecordPattern.java
+++ RecordPattern.java
@@ -1,6 +1,9 @@
 public class RecordPattern {
 
     record User(String firstName, String lastName){}
+    record ExtendedUser(String firstName, String lastName, Address address, Job job){}
+    record Address(String city, String street, int streetNumber){}
+    record Job(String jobTitle, String companyName){}
 
     public static void main(String[] args) { 
 
@@ -18,6 +21,18 @@ public class RecordPattern {
 
         if(user instanceof User(String firstName, String lastName)){
             System.out.println(firstName + " " + lastName);
+        }
+
+        Address address = new Address("city", "street", 1);
+        Job job = new Job("developer", "Software Company");
+        Object extendedUser = new ExtendedUser("firstName", "lastName", address, job);
+
+        if (extendedUser instanceof ExtendedUser(String firstName, String lastName, Address(String city, String street, int streetNumber), Job(String jobTitle, String companyName))) {
+            System.out.println("jobTitle: " + jobTitle);
+        }
+
+        if (extendedUser instanceof ExtendedUser u) {
+            System.out.println("jobTitle: " + u.job.jobTitle);
         }
     }
 
